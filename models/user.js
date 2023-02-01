8bcb6c03a56ae5cafbc8a3adde293694ab0cdadd
garden <bill990628@gmail.com>
2023-02-01 16:34:43 +0900

    bcrypt user password

--- models/user.js
+++ models/user.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const saltRounds = 10 //salt가 몇 글자인지 나타냄
+
 const userSchema = mongoose.Schema({
     name:{
         type: String,
@@ -27,6 +30,27 @@ const userSchema = mongoose.Schema({
     },
     tokenExp:{
         type: Number
+    }
+})
+
+userSchema.pre('save', function(next){
+    var user = this
+
+    // 비밀번호 변경시에만 암호화 해주기 위해
+    if(user.isModified('password')){
+        //비밀번호를 암호화 시킨다
+        bcrypt.genSalt(saltRounds, function(err, salt){
+            if(err) return next(err)
+
+            bcrypt.hash(user.password, salt, function(err, hash){
+                if(err) return next(err)
+                user.password = hash
+                next()
+            })
+        })
+    } else{
+        // 비밀번호 외 다른 정보들을 변경할 시에도 next로 보내줘야 한다
+        next()
     }
 })
 
